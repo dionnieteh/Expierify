518bde55b9cc2d4fc22451e51f0218f888a602f3
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-03-29 21:41:05 +0800

    added sign out button
    1. [bug] sign in does not let user choose account
    2. [bug] users are not logged in

--- app/src/main/java/com/example/expierify/ProfileFragment.java
+++ app/src/main/java/com/example/expierify/ProfileFragment.java
@@ -1,32 +1,31 @@
 package com.example.expierify;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private Button logOutButton;
+    private FirebaseAuth mAuth;
     public ProfileFragment() {
         // Required empty public constructor
     }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -58,7 +57,19 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        logOutButton = view.findViewById(R.id.logOutBtn);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(getContext(), "You are signed out", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getContext(),SignIn.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 }
