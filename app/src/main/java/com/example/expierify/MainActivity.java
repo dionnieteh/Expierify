3ce0dbd02e38966841e9ac9a10dd3ec3b783b606
NatalieNYX <yinxin1304@yahoo.com>
2023-03-30 18:09:26 +0800

    Store User in firebase realtime database

--- app/src/main/java/com/example/expierify/MainActivity.java
+++ app/src/main/java/com/example/expierify/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.expierify;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +10,13 @@ import android.widget.Toast;
 
 import com.example.expierify.databinding.ActivityMainBinding;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         FirebaseApp.initializeApp(this);
         ActivityMainBinding binding;
 
@@ -49,6 +60,40 @@ public class MainActivity extends AppCompatActivity {
             }
 
             return true;
+        });
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String email = currentUser.getEmail();
+
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("User");
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean emailExists = false;
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    User user = userSnapshot.getValue(User.class);
+                    if (user.email.equals(email)) {
+                        emailExists = true;
+                        break;
+                    }
+                }
+                if (!emailExists) {
+                    String name = currentUser.getDisplayName();
+                    String email = currentUser.getEmail();
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference usersRef = database.getReference("users");
+                    String uid = currentUser.getUid();
+                    User user = new User(name, email);
+                    usersRef.child(uid).setValue(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                startActivity(new Intent(MainActivity.this,  SignIn.class));
+            }
         });
 
     }
