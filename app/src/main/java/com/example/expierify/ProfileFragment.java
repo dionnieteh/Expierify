d82fbacbea628501cff00e3c323c1bc5182d354c
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-04-02 23:06:56 +0800

    display user info and save to db
    1. able to display google username, email, and profile picture
    2. save users' name and email in database

--- app/src/main/java/com/example/expierify/ProfileFragment.java
+++ app/src/main/java/com/example/expierify/ProfileFragment.java
@@ -1,23 +1,33 @@
 package com.example.expierify;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.load.data.mediastore.MediaStoreUtil;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -70,6 +80,22 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            TextView username = view.findViewById(R.id.UserName);
+            username.setText(name);
+            String email = user.getEmail();
+            TextView userEmail = view.findViewById(R.id.emailProfile);
+            userEmail.setText(email);
+            Uri photoUrl = user.getPhotoUrl();
+            ImageView profPic = view.findViewById(R.id.UserPicture);
+            // Load the image from a URL using Picasso
+            Picasso.get().load(photoUrl).into(profPic);
+        }
+
+
         logOutButton = view.findViewById(R.id.logOutBtn);
         logOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
