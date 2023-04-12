1a3f2708e5695ab05a597b47c8a70c5590c3c6ac
NatalieNYX <yinxin1304@yahoo.com>
2023-04-12 19:43:27 +0800

    notification

--- app/src/main/java/com/example/expierify/AdapterSubCategory.java
+++ app/src/main/java/com/example/expierify/AdapterSubCategory.java
@@ -40,8 +40,6 @@ public class AdapterSubCategory extends RecyclerView.Adapter<AdapterSubCategory.
     private ArrayList<String> foodIDList;
     private Context context;
     private ArrayList<Date> expiryList;
-    private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-    private String userID = currentUser.getUid();
 
     public AdapterSubCategory(ArrayList<Food> foodList, Context context, ArrayList<String> foodIDList) {
         this.foodList = foodList;
@@ -69,7 +67,6 @@ public class AdapterSubCategory extends RecyclerView.Adapter<AdapterSubCategory.
                     .placeholder(R.drawable.placeholderimg) // Set a placeholder image while the actual image is loading
                     .into(holder.foodImageView); // Set the image in the ImageView
         }
-       
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,9 +97,11 @@ public class AdapterSubCategory extends RecyclerView.Adapter<AdapterSubCategory.
 
     public void sortExpiryDateAscending() {
         ArrayList<Date> expiryList = new ArrayList<>();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("d/M/yyyy");
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
         for (Food food : foodList) {
-            if (food.getUserID().equals(userID)) { // Check if food userID matches current user's ID
+            if (food.getUserID().equals(currentUser.getUid())) { // Check if food userID matches current user's ID
                 try {
                     Date expiryDate = sdf.parse(food.getExpiry());
                     expiryList.add(expiryDate);
@@ -117,7 +116,7 @@ public class AdapterSubCategory extends RecyclerView.Adapter<AdapterSubCategory.
         ArrayList<String> sortedFoodIDList = new ArrayList<>();
         for (Date expiryDate : expiryList) {
             for (Food food : foodList) {
-                if (food.getUserID().equals(userID)) { // Check if food userID matches current user's ID
+                if (food.getUserID().equals(currentUser.getUid())) { // Check if food userID matches current user's ID
                     String expiryDateString = food.getExpiry();
                     DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
                     try {
@@ -164,4 +163,3 @@ public class AdapterSubCategory extends RecyclerView.Adapter<AdapterSubCategory.
         }
     }
 }
-
